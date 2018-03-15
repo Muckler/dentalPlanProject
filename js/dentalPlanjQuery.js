f38c7b7bfb3060ec45a8dad1e7e4ba84e1fffef3
Kristen Baldwin <kristenbaldwin@Kristens-MBP.localdomain>
2018-03-15 14:59:11 -0500

    Adding numscroller

--- js/dentalPlanjQuery.js
+++ js/dentalPlanjQuery.js
@@ -1,56 +1,56 @@
-"use strict";
-// Dan's java script
-
-$(".hidden").prop("disabled", true);
-
-// $("#yesKids").on("select", function() {
-//     console.log("select kids");
-//     $(".hidden").prop("disabled", false);
-// })
-
-// let kidsYN = $(".kids option:selected").text();
-// console.log(kidsYN);
-
-$(".kids").change(function() {
-    console.log(this);
-    if (this.value === "Yes") {
-        $(".hidden").prop("disabled", false);   
-    }
-
-})
-
-//this is a comment
-
-$("#submitButton").click(function() {
-    
-    let marriedYN = $("#maritalStatus option:selected").val();
-    let kidsYN = $("#kidsYN option:selected").val();
-    let orthoYN = $("#orthoYN option:selected").val();
-
-    if (marriedYN === "" || kidsYN === "") {
-        return alert("Please fill out form completely");
-    
-    }
-
-    let userInput = {married: false, kids: false, ortho: false};
-    
-    if(marriedYN === "Yes") {
-        userInput.married = true;
-    }
-
-    if (kidsYN === "Yes") {
-        userInput.kids = true;
-    }
-
-    if (orthoYN === "Yes") {
-        userInput.ortho = true;
-    }
-    
-    console.log(userInput);
-
-})
-
 (function () {
+    "use strict";
+    var userInput = {married: false, kids: false, ortho: false};
+
+    window.addEventListener('load', function() {
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+    }, false);
+
+    $(".hidden").prop("disabled", true);
+
+    $(".kids").change(function() {
+        console.log(this);
+        if (this.value === "Yes") {
+            $(".hidden").prop("disabled", false);   
+        } else {
+            $(".hidden").prop("disabled", true);   
+        }
+    })
+
+    $("#submitButton").click(function() {
+        console.log(userInput);
+        let marriedYN = $("#maritalStatus option:selected").val();
+        let kidsYN = $("#kidsYN option:selected").val();
+        let orthoYN = $("#orthoYN option:selected").val();
+        if (marriedYN === "" || kidsYN === "") {
+            console.log(userInput);
+            return alert("Please fill out form completely");
+        }
+        // let userInput = {married: false, kids: false, ortho: false};
+        if(marriedYN === "Yes") {
+            userInput.married = true;
+            console.log(userInput);
+        }
+        if (kidsYN === "Yes") {
+            userInput.kids = true;
+            console.log(userInput);
+        }
+        if (orthoYN === "Yes") {
+            userInput.ortho = true;
+            console.log(userInput);
+        }
+        console.log(userInput);
+    })
+
     //api url from https://dev.socrata.com/foundry/data.healthcare.gov/dtk6-f38y
     var url = "https://data.healthcare.gov/resource/dtk6-f38y.json";
     // API for Maryland schoolchildren data (Kindergarten  & 3rd grade)
@@ -65,15 +65,15 @@ $("#submitButton").click(function() {
         console.log(error);
     })
 
-        $.get(url2).done(function (response) {
-            let dentalStats = {};
-            let dentalVisit12Mos = response.data[32][9];
-            let privateDentalIns = response.data[29][9];
-            let toothAche12Mos = response.data[44][9];
-            dentalStats.dentalVisit12Mos = Number(dentalVisit12Mos);
-            dentalStats.privateDentalIns = Number(privateDentalIns);
-            dentalStats.toothAche12Mos = Number(toothAche12Mos);
-            $('#stats').innerHTML = '<span>' + dentalStats.dentalVisit12Mos + '</span>';
+    $.get(url2).done(function (response) {
+        let dentalStats = {};
+        let dentalVisit12Mos = response.data[32][9];
+        let privateDentalIns = response.data[29][9];
+        let toothAche12Mos = response.data[44][9];
+        dentalStats.dentalVisit12Mos = Number(dentalVisit12Mos);
+        dentalStats.privateDentalIns = Number(privateDentalIns);
+        dentalStats.toothAche12Mos = Number(toothAche12Mos);
+        $('#stats').innerHTML = '<span>' + dentalStats.dentalVisit12Mos + '</span>';
 
         }).fail(function (error) {
             console.log(error);
@@ -84,7 +84,7 @@ $("#submitButton").click(function() {
     console.log(userInput);
     //married function
     //below function for non married users
-    function Individual(userInput, uniqueS) 
+    function Individual(userInput, uniqueS) {
         var chartOutS = [];
         if (userInput.kids == true) {
             //user selected ortho coverage for kids
@@ -161,8 +161,7 @@ $("#submitButton").click(function() {
                     graphM.push(chartOutM);
                 }//end for loop
                 console.log(graphM);
-            }//end ortho els
-            
+            }//end ortho els   
         }//end kids if
         //else no kids  and therefor do not show ortho
         else {
@@ -177,7 +176,6 @@ $("#submitButton").click(function() {
                 graphM.push(chartOutM);
             }//end for 
             console.log(graphM);
-       
         }//end else
         return graphM;
     }//end married function
@@ -204,7 +202,7 @@ $("#submitButton").click(function() {
         var uniqueS = singleData.filter(
             function(a){if (!this[a.couple_1_child_age_21]) {this[a.couple_1_child_age_21] = 1; return a;}},
             {}
-           );
+        );
         console.log(uniqueS);
 
         if (userInput.married == true) {
@@ -213,8 +211,8 @@ $("#submitButton").click(function() {
         else {
             Individual(userInput, uniqueS);
         }
-
     }
+
     // handle XHR error
     function updateUIError() {
         var weatherBox = document.getElementById("dental");
